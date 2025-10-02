69fd7bed23427ae0924e8c9f2c3bc8b2dc3959a6
nahuel lozano <lozano.nahuel41@gmail.com>
2025-10-02 15:52:22 -0300

    se integra base de datos

--- senas_web/settings.py
+++ senas_web/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'senas_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'LsDB',    
+        'USER': 'userbd',
+        'PASSWORD': 'userdb',
+        'HOST': 'ALDYCASERES\SQLEXPRESS',       
+        'PORT': '1433',                 
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
     }
+}
 }
 
 
